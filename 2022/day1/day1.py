ea57d0d57672fde99308099859ffd478e6b55fe1
modelmasterio <58180596+modelmasterio@users.noreply.github.com>
2022-12-01 23:53:23 -0500

    part 2 of day 1 2022 problem

--- 2022/day1/day1.py
+++ 2022/day1/day1.py
@@ -1,19 +1,38 @@
 
 max_sum = 0
+second_sum = 0
+third_sum = 0
+
 current_elf_sum = 0
+
+elfUpdated = False
 
 data_file = open('day1.txt', 'r')
 raw_elf_data = data_file.readlines();
 
-for foodCalorieCount in raw_elf_data:
+for i in range(len(raw_elf_data)):
     
-    strippedCalorieCount = foodCalorieCount.strip();
-    
-    if strippedCalorieCount == '':
+    strippedCalorieCount = raw_elf_data[i].strip();
+
+    if strippedCalorieCount == '' or i == len(raw_elf_data)-1:
+        if i == len(raw_elf_data)-1:
+            current_elf_sum += int(strippedCalorieCount)
+        if current_elf_sum > max_sum:
+            third_sum = second_sum
+            second_sum = max_sum
+            max_sum = current_elf_sum
+        elif current_elf_sum > second_sum:
+            third_sum = second_sum
+            second_sum = current_elf_sum
+        elif current_elf_sum > third_sum:
+            third_sum = current_elf_sum
         current_elf_sum = 0
     else:
         current_elf_sum += int(strippedCalorieCount)
-        if current_elf_sum > max_sum:
-            max_sum = current_elf_sum
+        
 
 print(max_sum)
+print(second_sum)
+print(third_sum)
+
+print(max_sum+second_sum+third_sum)
